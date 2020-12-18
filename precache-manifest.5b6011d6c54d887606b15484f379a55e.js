3106b73b4300b2954a2716fcec08bc85504c06c5
atakori <atakori@outlook.com>
2020-12-18 14:59:57 -0500

    Updates

---
+++ precache-manifest.5b6011d6c54d887606b15484f379a55e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ee976e99c3b254a6370f15812c71a8",
+    "url": "/Simple-DataPoints-Table---React/index.html"
+  },
+  {
+    "revision": "7323c11422085351c678",
+    "url": "/Simple-DataPoints-Table---React/static/css/2.50a9db07.chunk.css"
+  },
+  {
+    "revision": "b4ca239bfef402684990",
+    "url": "/Simple-DataPoints-Table---React/static/css/main.7421b405.chunk.css"
+  },
+  {
+    "revision": "7323c11422085351c678",
+    "url": "/Simple-DataPoints-Table---React/static/js/2.4b6a35d6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Simple-DataPoints-Table---React/static/js/2.4b6a35d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ca239bfef402684990",
+    "url": "/Simple-DataPoints-Table---React/static/js/main.629eac69.chunk.js"
+  },
+  {
+    "revision": "252b84a038cb44acfc34",
+    "url": "/Simple-DataPoints-Table---React/static/js/runtime-main.76af75de.js"
+  }
+]);
